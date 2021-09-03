ca36b01ef7cda76d5c0dff6e6410a8f6f616d701
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-09-02 22:31:22 -0300

    organização das pasta e modularizando componentes

---
+++ src/app/shared/component/star/star.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { StarCompnent } from "./star.component";
+
+@NgModule({
+  declarations: [
+    StarCompnent
+  ], exports: [
+    StarCompnent
+  ]
+})
+export class StarModule {
+
+}
